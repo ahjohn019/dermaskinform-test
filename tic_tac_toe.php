899df90617c4b7266818362323acf8538b0d31ae
Ng Yew Rui <yewrui.ng@NeoAsia.local>
2020-11-09 11:57:39 +0800

    Fix Ip Address - SEVEN

--- tic_tac_toe.php
+++ tic_tac_toe.php
@@ -43,25 +43,9 @@
                 // $externalContent = file_get_contents('http://checkip.dyndns.com/');
                 // preg_match('/Current IP Address: \[?([:.0-9a-fA-F]+)\]?/', $externalContent, $m);
                 // $externalIp = $m[1];
-                $ipaddress = '';
-                if (getenv('HTTP_CLIENT_IP'))
-                    $ipaddress = getenv('HTTP_CLIENT_IP');
-                else if(getenv('HTTP_X_FORWARDED_FOR'))
-                    $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
-                else if(getenv('HTTP_X_FORWARDED'))
-                    $ipaddress = getenv('HTTP_X_FORWARDED');
-                else if(getenv('HTTP_FORWARDED_FOR'))
-                    $ipaddress = getenv('HTTP_FORWARDED_FOR');
-                else if(getenv('HTTP_FORWARDED'))
-                $ipaddress = getenv('HTTP_FORWARDED');
-                else if(getenv('REMOTE_ADDR'))
-                    $ipaddress = getenv('REMOTE_ADDR');
-                else
-                    $ipaddress = 'UNKNOWN';
-                return $ipaddress;
         ?>
         
-        <h3>Your IP Address : <?php echo $ipaddress;  ?></h3>
+        <h3>Your IP Address : <?php echo $_SERVER["REMOTE_ADDR"];  ?></h3>
         <h2>Round <p id="round_number" class="round_number"></p></h2>
     
         <table id="table_tictactoe">
