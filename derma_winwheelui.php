640f75f59dd79a46e9c07e97f88686f315d31244
Ng Yew Rui <yewrui.ng@NeoAsia.local>
2021-01-13 09:04:30 +0800

    Fix WinWheel UI JS Import Error

--- derma_winwheelui.php
+++ derma_winwheelui.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script type="text/javascript" src="Winwheel.min.js"></script>
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenMax.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenMax.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     
     <link rel="stylesheet" href="static/css/derma_winwheel.css">
